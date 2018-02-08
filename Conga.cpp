fd171979c2951b067996b4e6b632b6c2e190a9e8
soha4ever <hitomistar99@gmail.com>
2018-02-08 08:56:41 +0330

    movement function

--- Conga.cpp
+++ Conga.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 #include<vector>
+#include <utility>      // std::pair, std::make_pair
+
 #define Max 4
 using namespace std;
 //Container 
 struct Game_Grid{
     vector <vector<Grid_Cell> > Grid;
-    short int size;
+    short int size;//?
 };
 
 struct Player{
@@ -16,7 +18,7 @@ struct Player{
 struct Grid_Cell{
     struct Player player;
     short int Stone_Num;
-    bool IsInUse;
+    bool IsInUse; //true when the cell is already taken O.W flase
 };
 
 //create the gride 
@@ -94,4 +96,104 @@ vector<Game_Grid> successor(Game_Grid G , Player Turn){
         }
     }
     return Grid_List;
+}
+/*
+ ___ ___ ___ ___
+|_8_|_1_|_2_|___|
+|_7_|_c_|_3_|___|
+|_6_|_5_|_4_|___|
+|___|___|___|___|
+if c is Current location, numbers show direction
+
+*/
+Game_Grid move(Player turn, short int direction, pair<short int, short int> current_location, Game_Grid G)
+{
+	pair<short int, short int> next_step;
+	switch (direction)
+	{
+	case 1:
+		next_step.first = current_location.first;
+		next_step.second = current_location.second + 1;
+		break;
+	case 2:
+		next_step.first = current_location.first+1;
+		next_step.second = current_location.second + 1;
+		break;
+	case 3:
+		next_step.first = current_location.first + 1;
+		next_step.second = current_location.second;
+		break;
+	case 4:
+		next_step.first = current_location.first+1;
+		next_step.second = current_location.second - 1;
+		break;
+	case 5:
+		next_step.first = current_location.first;
+		next_step.second = current_location.second - 1;
+		break;
+	case 6:
+		next_step.first = current_location.first - 1;
+		next_step.second = current_location.second - 1;
+		break;
+	case 7:
+		next_step.first = current_location.first - 1;
+		next_step.second = current_location.second;
+		break;
+	case 8:
+		next_step.first = current_location.first - 1;
+		next_step.second = current_location.second + 1;
+		break;
+	}
+	short int stonesCount = 1;
+	short int allStones = G.Grid[current_location.first][current_location.second].Stone_Num;
+	while (0 <= next_step.first <= 4 && 0 <= next_step.second <= 4 && !G.Grid[next_step.first][next_step.second].IsInUse)
+	{
+		G.Grid[next_step.first][next_step.second].IsInUse = true;
+		G.Grid[next_step.first][next_step.second].player = turn;
+		current_location = next_step;
+		switch (direction)
+		{
+		case 1:
+			next_step.first = current_location.first;
+			next_step.second = current_location.second + 1;
+			break;
+		case 2:
+			next_step.first = current_location.first + 1;
+			next_step.second = current_location.second + 1;
+			break;
+		case 3:
+			next_step.first = current_location.first + 1;
+			next_step.second = current_location.second;
+			break;
+		case 4:
+			next_step.first = current_location.first + 1;
+			next_step.second = current_location.second - 1;
+			break;
+		case 5:
+			next_step.first = current_location.first;
+			next_step.second = current_location.second - 1;
+			break;
+		case 6:
+			next_step.first = current_location.first - 1;
+			next_step.second = current_location.second - 1;
+			break;
+		case 7:
+			next_step.first = current_location.first - 1;
+			next_step.second = current_location.second;
+			break;
+		case 8:
+			next_step.first = current_location.first - 1;
+			next_step.second = current_location.second + 1;
+			break;
+		}
+		if (0 <= next_step.first <= 4 && 0 <= next_step.second <= 4 && !G.Grid[next_step.first][next_step.second].IsInUse)
+		{
+			G.Grid[current_location.first][current_location.second].Stone_Num = stonesCount;
+			stonesCount++;
+		}
+		else
+			G.Grid[current_location.first][current_location.second].Stone_Num = allStones - stonesCount - 1;
+	}
+
+
 }
