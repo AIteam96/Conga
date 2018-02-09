2bf81c6d1f2dcf3366a54135b6469267c0e5c21d
mirzababa <34629367+mirzababa@users.noreply.github.com>
2018-02-09 02:51:54 -0800

    Add files via upload
    with Terminal Test

--- Conga.cpp
+++ Conga.cpp
@@ -1,105 +1,90 @@
-#include<iostream>
+﻿#include<iostream>
 #include<vector>
 #include <utility>      // std::pair, std::make_pair
 
 #define Max 4
 using namespace std;
-//Container 
-struct Game_Grid{
-    vector <vector<Grid_Cell> > Grid;
-    short int size;//?
-};
 
-struct Player{
-    short int Wich_Player; //1 , 2
-    short int Max_Depth;
+struct Player {
+	short int Wich_Player; //1 , 2
+	short int Max_Depth;
 	short int score;
 };
 
-struct Grid_Cell{
-    struct Player player;
-    short int Stone_Num;
-    bool IsInUse; //true when the cell is already taken O.W flase
+struct Grid_Cell {
+	struct Player player;
+	short int Stone_Num;
+	bool IsInUse; //true when the cell is already taken O.W flase
 };
-
+//Container 
+struct Game_Grid {
+	vector <vector<Grid_Cell> > Grid;
+};
 //create the gride 
-void Initial(){
-    Game_Grid G;
-    for (short int i = 0 ; i < Max ; i++){
-        for (short int j = 0 ; j < Max ; j++){
-            G.Grid[i][j].IsInUse = false;
-            G.Grid[i][j].Stone_Num = 0;
-        }
-    }
+void Initial() {
+	Game_Grid G;
+	for (short int i = 0; i < Max; i++) {
+		for (short int j = 0; j < Max; j++) {
+			G.Grid[i][j].IsInUse = false;
+			G.Grid[i][j].Stone_Num = 0;
+		}
+	}
 }
-    
-vector<Game_Grid> successor(Game_Grid G , Player Turn){
-    vector<Game_Grid> Grid_List;
-    Game_Grid Temp = G;
-    for (short int i = 0 ; i < Temp.size ; i++){
-        for (short int j = 0 ; j <Temp.size ; j++){
 
-            if(j < Temp.size && Temp.Grid[i][j+1].Wich_Player == turn || Temp.Grid[i][j+1].Wich_Player.IsInUse == false){
-                Temp.Grid[i][j+1].Stone_Num ++;
-                Temp.Grid[i][j+1].IsInUse = true;
-                Temp.Grid[i][j+1].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-
-            if(j > 0 && Temp.Grid[i][j-1].Wich_Player == turn || Temp.Grid[i][j-1].Wich_Player.IsInUse == false){
-                Temp.Grid[i][j-1].Stone_Num ++;
-                Temp.Grid[i][j-1].IsInUse = true;
-                Temp.Grid[i][j-1].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-            
-            if(i < Temp.size && Temp.Grid[i+1][j].Wich_Player == turn || Temp.Grid[i+1][j].Wich_Player.IsInUse == false){
-                Temp.Grid[i+1][j].Stone_Num ++;
-                Temp.Grid[i+1][j].IsInUse = true;
-                Temp.Grid[i+1][j].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-
-            if(i > 0 && Temp.Grid[i-1][j].Wich_Player == turn || Temp.Grid[i-1][j].Wich_Player.IsInUse == false){
-                Temp.Grid[i-1][j].Stone_Num ++;
-                Temp.Grid[i-1][j].IsInUse = true;
-                Temp.Grid[i-1][j].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-
-            if((i > 0 &&  j > 0) && Temp.Grid[i-1][j-1].Wich_Player == turn || Temp.Grid[i-1][j-1].Wich_Player.IsInUse == false){
-                Temp.Grid[i-1][j-1].Stone_Num ++;
-                Temp.Grid[i-1][j-1].IsInUse = true;
-                Temp.Grid[i-1][j-1].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-
-            if((i < Temp.size && j < Temp.size ) && Temp.Grid[i+1][j-1].Wich_Player == turn || Temp.Grid[i+1][j-1].Wich_Player.IsInUse == false){
-                Temp.Grid[i+1][j-1].Stone_Num ++;
-                Temp.Grid[i+1][j-1].IsInUse = true;
-                Temp.Grid[i+1][j-1].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-
-            if((i > 0 && j < Temp.size ) && Temp.Grid[i-1][j+1].Wich_Player == turn || Temp.Grid[i-1][j+1].Wich_Player.IsInUse == false){
-                Temp.Grid[i-1][j+1].Stone_Num ++;
-                Temp.Grid[i-1][j+1].IsInUse = true;
-                Temp.Grid[i-1][j+1]].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-
-            if((i < Temp.size && j < Temp.size ) && Temp.Grid[i+1][j+1].Wich_Player == turn || Temp.Grid[i+1][j+1].Wich_Player.IsInUse == false){
-                Temp.Grid[i+1][j+1].Stone_Num ++;
-                Temp.Grid[i+1][j+1].IsInUse = true;
-                Temp.Grid[i+1][j+1]].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-        }
-    }
-    return Grid_List;
+vector<Game_Grid> successor(Game_Grid G, Player Turn) {
+	vector<Game_Grid> Grid_List;
+	Game_Grid Temp = G;
+	pair<short int, short int> Current;
+	for (short int i = 0; i < Max; i++) {
+		for (short int j = 0; j < Max; j++) {
+			Current.first = i;
+			Current.second = j;
+			if (i > 0)
+			{
+				Temp = move(Turn, 1, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (i > 0 && j > 0)
+			{
+				Temp = move(Turn, 2, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (j > 0)
+			{
+				Temp = move(Turn, 3, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (i < Max && j < Max)
+			{
+				move(Turn, 4, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (i < Max)
+			{
+				Temp = move(Turn, 5, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (i < Max && j > 0)
+			{
+				Temp = move(Turn, 6, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (j < Max)
+			{
+				Temp = move(Turn, 7, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (i > 0 && j < Max)
+			{
+				Temp = move(Turn, 8, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+		}
+	}
+	return Grid_List;
 }
 /*
- ___ ___ ___ ___
+___ ___ ___ ___
 |_8_|_1_|_2_|___|
 |_7_|_c_|_3_|___|
 |_6_|_5_|_4_|___|
@@ -107,8 +92,7 @@ vector<Game_Grid> successor(Game_Grid G , Player Turn){
 if c is Current location, numbers show direction
 
 */
-Game_Grid move(Player turn, short int direction, pair<short int, short int> current_location, Game_Grid G)
-{
+Game_Grid move(Player Turn, short int direction, pair<short int, short int> current_location, Game_Grid G) {
 	pair<short int, short int> next_step;
 	switch (direction)
 	{
@@ -117,7 +101,7 @@ Game_Grid move(Player turn, short int direction, pair<short int, short int> curr
 		next_step.second = current_location.second + 1;
 		break;
 	case 2:
-		next_step.first = current_location.first+1;
+		next_step.first = current_location.first + 1;
 		next_step.second = current_location.second + 1;
 		break;
 	case 3:
@@ -125,7 +109,7 @@ Game_Grid move(Player turn, short int direction, pair<short int, short int> curr
 		next_step.second = current_location.second;
 		break;
 	case 4:
-		next_step.first = current_location.first+1;
+		next_step.first = current_location.first + 1;
 		next_step.second = current_location.second - 1;
 		break;
 	case 5:
@@ -147,10 +131,10 @@ Game_Grid move(Player turn, short int direction, pair<short int, short int> curr
 	}
 	short int stonesCount = 1;
 	short int allStones = G.Grid[current_location.first][current_location.second].Stone_Num;
-	while (0 <= next_step.first <= 4 && 0 <= next_step.second <= 4 && !G.Grid[next_step.first][next_step.second].IsInUse)
+	while (0 < next_step.first < 4 && 0 < next_step.second < 4 && !G.Grid[next_step.first][next_step.second].IsInUse || G.Grid[next_step.first][next_step.second].player.Wich_Player == Turn.Wich_Player)
 	{
 		G.Grid[next_step.first][next_step.second].IsInUse = true;
-		G.Grid[next_step.first][next_step.second].player = turn;
+		G.Grid[next_step.first][next_step.second].player = Turn;
 		current_location = next_step;
 		switch (direction)
 		{
@@ -187,7 +171,7 @@ Game_Grid move(Player turn, short int direction, pair<short int, short int> curr
 			next_step.second = current_location.second + 1;
 			break;
 		}
-		if (0 <= next_step.first <= 4 && 0 <= next_step.second <= 4 && !G.Grid[next_step.first][next_step.second].IsInUse)
+		if (0 < next_step.first < 4 && 0 < next_step.second < 4 && !G.Grid[next_step.first][next_step.second].IsInUse)
 		{
 			G.Grid[current_location.first][current_location.second].Stone_Num = stonesCount;
 			stonesCount++;
@@ -195,13 +179,52 @@ Game_Grid move(Player turn, short int direction, pair<short int, short int> curr
 		else
 			G.Grid[current_location.first][current_location.second].Stone_Num = allStones - stonesCount - 1;
 	}
-		return G;
+	return G;
 }
-float MaxVaue(Game_Grid G,Player player)
-{
-	 
-}
-Game_Grid minimax(Game_Grid G, Player player)
-{
+float MaxVaue(Game_Grid G, Player player) {
 
+}
+Game_Grid minimax(Game_Grid G, Player player) {
+
+}
+
+short int Utility(Game_Grid G, Player player, pair<short int, short int> current_location) {
+
+}
+// بعد از هر حرکت بازیکن چک میشود که آیا حریف در حرکت قبلی خود تمام مهره های خود را استفاده کرده است یا خیر. اگر استفاده کرده بود حریف بازنده میشود
+bool TerminalCheck1(Game_Grid G, Player player) {
+	for (short int i = 0; i < Max; i++) {
+		for (short int j = 0; j < Max; j++) {
+			if (G.Grid[i][j].player.Wich_Player != player.Wich_Player) {
+				if (G.Grid[i][j].Stone_Num <= 0) { return true; }
+			}
+		}
+	}
+}
+// بعد از انجام هر مرحله چک میشود که آیا خانه هایی که حریف در آنها مهره دارد، راه برای حرکت دارد یا ندارد درواقع حریف را محاصره کرده یا نه
+short int TerminalCheck2(Game_Grid G, Player player) {
+	short int way = 8;
+	for (short int i = 0; i < Max; i++) {
+		for (short int j = 0; j < Max; j++) {
+			if (G.Grid[i][j].player.Wich_Player != player.Wich_Player) {
+				//direction 1
+				if (G.Grid[i - 1][j].player.Wich_Player == player.Wich_Player) { way--; }
+				//direction 2
+				if (G.Grid[i + 1][j - 1].player.Wich_Player == player.Wich_Player) { way--; }
+				//direction 3
+				if (G.Grid[i][j - 1].player.Wich_Player == player.Wich_Player) { way--; }
+				//direction 4
+				if (G.Grid[i + 1][j - 1].player.Wich_Player == player.Wich_Player) { way--; }
+				//direction 5
+				if (G.Grid[i + 1][j].player.Wich_Player == player.Wich_Player) { way--; }
+				//direction 6
+				if (G.Grid[i + 1][j + 1].player.Wich_Player == player.Wich_Player) { way--; }
+				//direction 7
+				if (G.Grid[i][j + 1].player.Wich_Player == player.Wich_Player) { way--; }
+				//direction 8
+				if (G.Grid[i - 1][j + 1].player.Wich_Player == player.Wich_Player) { way--; }
+			}
+		}
+	}
+	return way;
 }
