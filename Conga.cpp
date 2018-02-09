461c4dab9e0bdda383845333eeaa336ceb4038e3
soha4ever <hitomistar99@gmail.com>
2018-02-09 11:47:32 +0330

    simple minimax first try

--- Conga.cpp
+++ Conga.cpp
@@ -2,104 +2,91 @@
 #include<vector>
 #include <utility>      // std::pair, std::make_pair
 
-#define Max 4
+#define MAX 4
 using namespace std;
 //Container 
-struct Game_Grid{
-    vector <vector<Grid_Cell> > Grid;
-    short int size;//?
+struct Game_Grid {
+	vector <vector<Grid_Cell> > Grid;
 };
 
-struct Player{
-    short int Wich_Player; //1 , 2
-    short int Max_Depth;
+struct Player {
+	short int Wich_Player; //1 , 2
+	short int MAX_Depth;
 	short int score;
 };
 
-struct Grid_Cell{
-    struct Player player;
-    short int Stone_Num;
-    bool IsInUse; //true when the cell is already taken O.W flase
+struct Grid_Cell {
+	struct Player player;
+	short int Stone_Num;
+	bool IsInUse; //true when the cell is already taken O.W flase
 };
 
 //create the gride 
-void Initial(){
-    Game_Grid G;
-    for (short int i = 0 ; i < Max ; i++){
-        for (short int j = 0 ; j < Max ; j++){
-            G.Grid[i][j].IsInUse = false;
-            G.Grid[i][j].Stone_Num = 0;
-        }
-    }
+void Initial() {
+	Game_Grid G;
+	for (short int i = 0; i < MAX; i++) {
+		for (short int j = 0; j < MAX; j++) {
+			G.Grid[i][j].IsInUse = false;
+			G.Grid[i][j].Stone_Num = 0;
+		}
+	}
 }
-    
-vector<Game_Grid> successor(Game_Grid G , Player Turn){
-    vector<Game_Grid> Grid_List;
-    Game_Grid Temp = G;
-    for (short int i = 0 ; i < Temp.size ; i++){
-        for (short int j = 0 ; j <Temp.size ; j++){
 
-            if(j < Temp.size && Temp.Grid[i][j+1].Wich_Player == turn || Temp.Grid[i][j+1].Wich_Player.IsInUse == false){
-                Temp.Grid[i][j+1].Stone_Num ++;
-                Temp.Grid[i][j+1].IsInUse = true;
-                Temp.Grid[i][j+1].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
+vector<Game_Grid> successor(Game_Grid G, Player Turn) {
+	vector<Game_Grid> Grid_List;
+	Game_Grid Temp = G;
+	pair<short int, short int> Current;
 
-            if(j > 0 && Temp.Grid[i][j-1].Wich_Player == turn || Temp.Grid[i][j-1].Wich_Player.IsInUse == false){
-                Temp.Grid[i][j-1].Stone_Num ++;
-                Temp.Grid[i][j-1].IsInUse = true;
-                Temp.Grid[i][j-1].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-            
-            if(i < Temp.size && Temp.Grid[i+1][j].Wich_Player == turn || Temp.Grid[i+1][j].Wich_Player.IsInUse == false){
-                Temp.Grid[i+1][j].Stone_Num ++;
-                Temp.Grid[i+1][j].IsInUse = true;
-                Temp.Grid[i+1][j].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-
-            if(i > 0 && Temp.Grid[i-1][j].Wich_Player == turn || Temp.Grid[i-1][j].Wich_Player.IsInUse == false){
-                Temp.Grid[i-1][j].Stone_Num ++;
-                Temp.Grid[i-1][j].IsInUse = true;
-                Temp.Grid[i-1][j].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-
-            if((i > 0 &&  j > 0) && Temp.Grid[i-1][j-1].Wich_Player == turn || Temp.Grid[i-1][j-1].Wich_Player.IsInUse == false){
-                Temp.Grid[i-1][j-1].Stone_Num ++;
-                Temp.Grid[i-1][j-1].IsInUse = true;
-                Temp.Grid[i-1][j-1].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-
-            if((i < Temp.size && j < Temp.size ) && Temp.Grid[i+1][j-1].Wich_Player == turn || Temp.Grid[i+1][j-1].Wich_Player.IsInUse == false){
-                Temp.Grid[i+1][j-1].Stone_Num ++;
-                Temp.Grid[i+1][j-1].IsInUse = true;
-                Temp.Grid[i+1][j-1].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-
-            if((i > 0 && j < Temp.size ) && Temp.Grid[i-1][j+1].Wich_Player == turn || Temp.Grid[i-1][j+1].Wich_Player.IsInUse == false){
-                Temp.Grid[i-1][j+1].Stone_Num ++;
-                Temp.Grid[i-1][j+1].IsInUse = true;
-                Temp.Grid[i-1][j+1]].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-
-            if((i < Temp.size && j < Temp.size ) && Temp.Grid[i+1][j+1].Wich_Player == turn || Temp.Grid[i+1][j+1].Wich_Player.IsInUse == false){
-                Temp.Grid[i+1][j+1].Stone_Num ++;
-                Temp.Grid[i+1][j+1].IsInUse = true;
-                Temp.Grid[i+1][j+1]].Wich_Player = Turn;
-                Grid_List.push_back(Temp);
-            }
-        }
-    }
-    return Grid_List;
+	for (short int i = 0; i < MAX; i++) {
+		for (short int j = 0; j < MAX; j++) {
+			Current.first = i;
+			Current.second = j;
+			if (i > 1)
+			{
+				Temp = move(Turn, 1, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (i > 1 && j > 1)
+			{
+				Temp = move(Turn, 2, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (j > 1)
+			{
+				Temp = move(Turn, 3, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (i < MAX && j < MAX)
+			{
+				move(Turn, 4, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (i < MAX)
+			{
+				Temp = move(Turn, 5, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (i < MAX && j > 1)
+			{
+				Temp = move(Turn, 6, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (j < MAX)
+			{
+				Temp = move(Turn, 7, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+			if (i > 1 && j < MAX)
+			{
+				Temp = move(Turn, 8, Current, Temp);
+				Grid_List.push_back(Temp);
+			}
+		}
+	}
+	return Grid_List;
 }
 /*
- ___ ___ ___ ___
+___ ___ ___ ___
 |_8_|_1_|_2_|___|
 |_7_|_c_|_3_|___|
 |_6_|_5_|_4_|___|
@@ -107,7 +94,7 @@ vector<Game_Grid> successor(Game_Grid G , Player Turn){
 if c is Current location, numbers show direction
 
 */
-Game_Grid move(Player turn, short int direction, pair<short int, short int> current_location, Game_Grid G)
+Game_Grid move(Player Turn, short int direction, pair<short int, short int> current_location, Game_Grid G)
 {
 	pair<short int, short int> next_step;
 	switch (direction)
@@ -117,7 +104,7 @@ Game_Grid move(Player turn, short int direction, pair<short int, short int> curr
 		next_step.second = current_location.second + 1;
 		break;
 	case 2:
-		next_step.first = current_location.first+1;
+		next_step.first = current_location.first + 1;
 		next_step.second = current_location.second + 1;
 		break;
 	case 3:
@@ -125,7 +112,7 @@ Game_Grid move(Player turn, short int direction, pair<short int, short int> curr
 		next_step.second = current_location.second;
 		break;
 	case 4:
-		next_step.first = current_location.first+1;
+		next_step.first = current_location.first + 1;
 		next_step.second = current_location.second - 1;
 		break;
 	case 5:
@@ -147,10 +134,10 @@ Game_Grid move(Player turn, short int direction, pair<short int, short int> curr
 	}
 	short int stonesCount = 1;
 	short int allStones = G.Grid[current_location.first][current_location.second].Stone_Num;
-	while (0 <= next_step.first <= 4 && 0 <= next_step.second <= 4 && !G.Grid[next_step.first][next_step.second].IsInUse)
+	while (0 <= next_step.first <= 4 && 0 <= next_step.second <= 4 && !G.Grid[next_step.first][next_step.second].IsInUse || G.Grid[next_step.first][next_step.second].player.Wich_Player == Turn.Wich_Player)
 	{
 		G.Grid[next_step.first][next_step.second].IsInUse = true;
-		G.Grid[next_step.first][next_step.second].player = turn;
+		G.Grid[next_step.first][next_step.second].player = Turn;
 		current_location = next_step;
 		switch (direction)
 		{
@@ -195,13 +182,56 @@ Game_Grid move(Player turn, short int direction, pair<short int, short int> curr
 		else
 			G.Grid[current_location.first][current_location.second].Stone_Num = allStones - stonesCount - 1;
 	}
-		return G;
+	return G;
 }
-float MaxVaue(Game_Grid G,Player player)
-{
-	 
-}
-Game_Grid minimax(Game_Grid G, Player player)
-{
 
+float MaxValue(Game_Grid G, Player maxplayer, Player minplayer,Game_Grid &best_Grid)
+{
+	 //if(!Terminal(G)){
+	vector<Game_Grid> actions = successor(G, maxplayer);
+	float resultVlue = -2.00;
+	int i = 0;
+	while (!actions.empty())
+	{
+		Game_Grid current_State = actions[i];i++;
+		float currentRating = MinValue(G, maxplayer, minplayer, current_State);
+		if (currentRating > resultVlue)
+		{
+			resultVlue = currentRating;
+			best_Grid = current_State;
+		}
+	}
+	return resultVlue;
+	//}
+	/*else
+		return player.score;
+	*/
+}
+float MinValue(Game_Grid G, Player maxplayer, Player minplayer, Game_Grid &best_Grid)
+{
+	//if(!Terminal(G)){
+	vector<Game_Grid> actions = successor(G, minplayer);
+	float resultVlue = +2.00;
+	int i = 0;
+	while (!actions.empty())
+	{
+		Game_Grid current_State = actions[i]; i++;
+		float currentRating = MaxValue(G, maxplayer, minplayer, current_State);
+		if (currentRating < resultVlue)
+		{
+			resultVlue = currentRating;
+			best_Grid = current_State;
+		}
+	}
+	return resultVlue;
+	//}
+	/*else
+	return player.score;
+	*/
+}
+Game_Grid miniMAX(Game_Grid G, Player player1, Player player2)
+{
+	Game_Grid best_Grid = G;
+	float i = MaxValue(G, player1, player2, best_Grid);
+	return best_Grid;
 }
